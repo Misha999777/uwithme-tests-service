4cfe8c8aeede50150d952b5756d7061e5d53d0e1
mariia.borodin <m.borodin.1999@gmail.com>
2022-06-12 19:21:34 +0200

    update properties [skip ci]

--- testsystem-ui/src/environments/environment.ts
+++ testsystem-ui/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  home: "http://localhost:8081",
-  backend: "http://localhost:8081",
+  home: "http://localhost:8090",
+  backend: "http://localhost:8090",
   keycloak: "http://localhost:8080/auth/realms/test"
 };
 
