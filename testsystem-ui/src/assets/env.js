1f10f176b0039d35169882fe6b2c376aa846aacb
Mykhailo Hroshevyi <misha999777@gmail.com>
2022-11-08 23:37:35 +0200

    fixed properties

--- testsystem-ui/src/assets/env.js
+++ testsystem-ui/src/assets/env.js
@@ -1,4 +1,4 @@
 window["env"] = window["env"] || {};
 
-window["env"]["SERVER_URL"] = "http://localhost:8081";
+window["env"]["SERVER_URL"] = "http://localhost:8090";
 window["env"]["KEYCLOAK_URL"] = "http://localhost:8080/auth/realms/test";
