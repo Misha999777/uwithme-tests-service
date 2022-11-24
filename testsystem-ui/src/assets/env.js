3ec153efbc716d0992343d80dee099c851980efe
Mykhailo Hroshevyi <misha999777@gmail.com>
2022-11-24 02:43:57 +0100

    updated properties

--- testsystem-ui/src/assets/env.js
+++ testsystem-ui/src/assets/env.js
@@ -1,4 +1,6 @@
 window["env"] = window["env"] || {};
 
+window["env"]["DEV_MODE"] = true;
+
 window["env"]["SERVER_URL"] = "http://localhost:8090";
 window["env"]["KEYCLOAK_URL"] = "http://localhost:8080/auth/realms/test";
--- testsystem-ui/src/main.ts
+++ testsystem-ui/src/main.ts
@@ -2,9 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {constants} from "./constants/constants";
 
-if (environment.production) {
+if (!constants.devMode) {
   enableProdMode();
 }
 
